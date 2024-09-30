e91e42a4019d90dd5654b2d402c3af040d12c18e
Mahlon Reese <78235036+mahlonreese@users.noreply.github.com>
2024-09-30 16:30:31 -0400

    Update Model.cs

--- Model.cs
+++ Model.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
-
+//maybe some alias' for better readability
 public class Movie
 {
     public int MovieId { get; set; }
